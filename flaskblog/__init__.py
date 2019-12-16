0027d18deee87ee26e16f36d472915a0f8e7b8cf
Anish <anish.jangra1654@outlook.com>
2019-12-16 23:17:36 +0530

    password reset option included

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2b057aa484be99fc6e7b5c45ef8d70a4'
@@ -11,6 +13,15 @@ bcrypt = Bcrypt(app);
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
+
+
+mail = Mail(app)
 
 
 from flaskblog import routes
